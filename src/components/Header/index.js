1fa8850aa761d22dbd628e7a0ed24c4aee1b0915
Nataly Dugasova <dugasova@gmail.com>
2022-12-24 14:54:39 +0200

    Homework #21

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import Logo from "./Logo";
+import Menu from "./Menu";
+import Title from "./Title";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <Logo />
+      <Title />
+      <Menu  />
+    </HeaderStyled>
+  );
+};
+  
+const HeaderStyled = styled.div({
+  background: "linear-gradient(#bc581e, #e27b36)",
+  height: "15vh",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default Header;
---
+++ src/components/Main/Prices/SingglePrice/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+const SinglePrice = ({singleName, singlePrice}) =>{
+    return (
+        <SinglePriceStyled>
+            {singleName}: {singlePrice} UAH
+        </SinglePriceStyled>
+    );
+};
+
+
+const SinglePriceStyled = styled.div({
+    textAlign: " center",
+    margin:" 20px 20px",
+    fontSize: "20px"
+})
+export default SinglePrice;
