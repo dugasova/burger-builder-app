98137db74df4e1126223abf56a8181c3b0d6653d
Nataly Dugasova <dugasova@gmail.com>
2022-12-24 19:03:43 +0200

    Fix proposed issues

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -48,7 +48,7 @@ class Main extends React.Component {
         event.preventDefault();
         const ingredientChoosed = event.target.parentNode.dataset["ingredient"] || event.target.dataset["ingredient"]
 
-        if ( !event.target.dataset["action"] || !event.target.dataset["ingredient"]) {
+        if (!event.target.dataset["action"] || !event.target.dataset["ingredient"]) {
             return
         }
 
