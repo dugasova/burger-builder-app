ecff088c5ce008d72747ed9cec935a8ed165fecc
Nataly Dugasova <dugasova@gmail.com>
2023-01-07 16:19:08 +0200

    Homework #24

---
+++ src/components/Checkout/index.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import ShoppingCartCheckoutOutlinedIcon from "@mui/icons-material/ShoppingCartCheckoutOutlined";
+import { useState } from "react";
+import CheckoutModal from "../CheckoutModal";
+import { Button } from "@mui/material";
+
+const style = {
+    backgroundColor: "chocolate",
+    display: "flex",
+    justifyContent: "center",
+    width: "160px",
+    height: "50px",
+    borderRadius: "20%",
+    boxShadow: "2px 2px 2px #fff",
+    marginBottom: "200px"
+  }
+
+
+const Checkout = ({
+    orderSummary,
+    orderedPrice,
+    handleFastDelivery,
+    clearBurger,
+}) => {
+
+    const [checkoutModalOpen, setCheckouModalOpen] = useState(false);
+    
+    const CheckoutModalClose = () => {
+        setCheckouModalOpen(false);
+    };
+
+    const handleCheckoutClick = async => {
+        setCheckouModalOpen(true);
+    };
+
+    return (
+        <CheckoutStyled>
+            {checkoutModalOpen && (
+                <CheckoutModal
+                    orderSummary={orderSummary}
+                    orderedPrice={orderedPrice}
+                    isOpen={checkoutModalOpen}
+                    handleClose={CheckoutModalClose}
+                    handleFastDelivery={handleFastDelivery}
+                    clearBurger={clearBurger}
+                />
+            )}
+            
+            <Button
+                onClick={+orderedPrice > 1 ? handleCheckoutClick : undefined}
+                variant="contained"
+                size="large"
+                style={style}
+                endIcon={<ShoppingCartCheckoutOutlinedIcon />}
+                >Checkout</Button>
+        </CheckoutStyled>
+    );
+};
+
+const CheckoutStyled = styled.div({
+    display: "flex",
+    justifyContent: "space-around",
+    marginTop: "50px"
+});
+
+export default Checkout;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,50 +1,40 @@
-import React from "react";
 import axios from "axios";
 import styled from "styled-components";
 import IngredientControl from "./IngredientControl";
 import Prices from "./Prices";
 import Burger from "./Burger";
+import React, { useState, useEffect } from "react";
+import Checkout from "../Checkout";
 
-class Main extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            loading: true,
-            prices: [],
-            ingredients: [],
-            ingredientsOrder: [],
-            burgerBuilder: {},
-            orderedPrice: "1.00",
-        };
-    }
+function Main () {
+    const [prices, setPrices] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
+    const [ingredientsOrder, setIngredientsOrder] = useState([]);
+    const [burgerBuilder, setBurgerBuilder] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [orderedPrice, setOrderedPrice] = useState('1.00')
     
-    findSinglePrice = (ingredient) => {
-        return this.state.prices.find(
+    useEffect ( () => {
+        (async() => {
+            axios("https://burger-api-xcwp.onrender.com/ingredients")
+                .then((res) => {
+                    const data = res.data
+                    setPrices(data)
+                    setIngredients(data.map((ingredient) => ingredient.name))
+                    setBurgerBuilder(data.reduce((acc, current) => ({[current.name]: 0, ...acc}), {} ))
+                    setLoading(false)
+                })
+                .catch( (err) => { console.error(err)})
+        })()
+    }, [])
+
+    const findSinglePrice = (ingredient) => {
+        return prices.find(
             (price) => price.name === ingredient
             ).price;
     };
     
-    componentDidMount = async () => {
-        try {
-            this.setState({loading: true});
-
-            const { data } = await axios.get("https://burger-api-xcwp.onrender.com/ingredients")
-            
-            const ingredients = data.map((ingredient) => ingredient.name)
-            const quantities = data.reduce((acc, current) => ({[current.name]: 0, ...acc}), {} )
-            this.setState({
-                prices: data,
-                ingredients: ingredients,
-                burgerBuilder: quantities,
-                loading: false,
-            })
-        }
-        catch (error) {
-            console.error(error)
-        }
-    }
-    
-    changeIngredientQuantity = (event) => {
+    const changeIngredientQuantity = (event) => {
         event.preventDefault();
         const ingredientChoosed = event.target.parentNode.dataset["ingredient"] || event.target.dataset["ingredient"]
 
@@ -53,70 +43,78 @@ class Main extends React.Component {
         }
 
         const actionChoosed = event.target.dataset["action"] || event.target.parentNode.dataset["action"]
-        const ingredientPrice = this.findSinglePrice(ingredientChoosed);
+        const ingredientPrice = findSinglePrice(ingredientChoosed);
 
-        this.setState ( (prevState) => {
-            const copyBurgerBuilder = {...prevState.burgerBuilder};
-            const copyIngredientsOrder = [...prevState.ingredientsOrder] ;
+        const copyBurgerBuilder = {...burgerBuilder}
+        const copyIngredientsOrder = [...ingredientsOrder]
 
-            let newPrice = +prevState.orderedPrice;
-            if (actionChoosed === "decrement") {
-                if (copyBurgerBuilder[ingredientChoosed] > 0) {
-                    newPrice -= +ingredientPrice;
-                    const index = copyIngredientsOrder.lastIndexOf(ingredientChoosed);
-                    copyIngredientsOrder.splice(index, 1);
-                    copyBurgerBuilder[ingredientChoosed]--;
-                }
+        let newPrice = +orderedPrice;
+        if (actionChoosed === "decrement") {
+            if (copyBurgerBuilder[ingredientChoosed] > 0) {
+                newPrice -= +ingredientPrice;
+                const index = copyIngredientsOrder.lastIndexOf(ingredientChoosed);
+                copyIngredientsOrder.splice(index, 1);
+                copyBurgerBuilder[ingredientChoosed]--;
             }
+        }
 
-            if (actionChoosed === "increment") {
-                if (copyBurgerBuilder[ingredientChoosed] < 5 && copyIngredientsOrder.length < 10) {
-                    newPrice += +ingredientPrice;
-                    copyIngredientsOrder.push(ingredientChoosed);
-                    copyBurgerBuilder[ingredientChoosed]++;
-                }
+        if (actionChoosed === "increment") {
+            if (copyBurgerBuilder[ingredientChoosed] < 5 && copyIngredientsOrder.length < 10) {
+                newPrice += +ingredientPrice;
+                copyIngredientsOrder.push(ingredientChoosed);
+                copyBurgerBuilder[ingredientChoosed]++;
             }
-            return {
-                ...prevState,
-                burgerBuilder: copyBurgerBuilder,
-                ingredientsOrder: copyIngredientsOrder,
-                orderedPrice: newPrice.toFixed(2),
-            }
-        })
+        }
+        setBurgerBuilder(copyBurgerBuilder)
+        setIngredientsOrder(copyIngredientsOrder)
+        setOrderedPrice(newPrice.toFixed(2))
     }
 
-    clearBurger = ()=> {
+    const clearBurger = () => {
         const clearBurgerBuilder = {}
-        for (const ingredient in this.state.burgerBuilder) {
+        for (const ingredient in burgerBuilder) {
             clearBurgerBuilder[ingredient] = 0;
         }
-        if (this.state.ingredientsOrder.length !== 0) {
-            this.setState({
-                ingredientsOrder: [],
-                burgerBuilder: clearBurgerBuilder,
-                orderedPrice : "1.00"
-            });
+        if (ingredientsOrder.length !== 0) {
+                setIngredientsOrder([])
+                setBurgerBuilder(clearBurgerBuilder)
+                setOrderedPrice("1.00")
         }
     };
 
-    render () {
-        const {prices, ingredients, burgerBuilder, loading, ingredientsOrder, orderedPrice} = this.state;
-        return (
+    const handleFastDelivery = (fastDelivery) => {
+        if (fastDelivery) {
+            setOrderedPrice((+orderedPrice + 2.5).toFixed(2))
+        } else {
+            setOrderedPrice((+orderedPrice - 2.5).toFixed(2))
+          }
+    }
+
+    return (
+        <>
             <MainWrapper>
                 <Prices allPrices={prices} loading={loading}/>
                 <Burger 
-                ingredientsOrder={ingredientsOrder}
-                totalPrice={orderedPrice}  />
+                    ingredientsOrder={ingredientsOrder}
+                    totalPrice={orderedPrice}  
+                />
                 <IngredientControl
                     prices={prices}
                     ingredients = {ingredients}
-                    updataBurger = {this.changeIngredientQuantity}
+                    updataBurger = {changeIngredientQuantity}
                     burgerIngredients = {burgerBuilder}
-                    clearBurger={this.clearBurger}
+                    clearBurger={clearBurger}
                     loading={loading} />
             </MainWrapper>
-        )
-    }
+            <Checkout
+                    orderSummary={burgerBuilder}
+                    orderedPrice={orderedPrice}
+                    handleFastDelivery={handleFastDelivery}
+                    clearBurger={clearBurger}
+            />                
+        </>
+
+    )
 }
 
 const MainWrapper = styled.div({
@@ -128,5 +126,6 @@ const MainWrapper = styled.div({
     height: "80%",
     alignContent: "center",
 });
+
 
 export default Main;
